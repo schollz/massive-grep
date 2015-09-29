2d58d9a9fdd6c22c83e5e56b660d308b5112800b
Zack Scholl <zack.scholl@gmail.com>
2015-09-29 13:57:03 -0400

    Added simple 1-processor method and multi-processor method

--- lib.py
+++ lib.py
@@ -25,26 +25,38 @@ def bzgrep(dat):
 	data = out.decode('utf-8')
 	return data
 
-def searchAll(word):
-	files = [y for x in os.walk('./') for y in glob(os.path.join(x[0], '*.bz2'))]
-	dat = []
-	for file in files:
-		dat.append((word,file))
 
-	p=Pool(8)
-	results  = p.map(bzgrep,dat)
-	datas = []
-	for result in results:
-		for res in result.split('\n'):
-			try:
-				datas.append(json.loads(res))
-			except:
-				pass
-	return datas
+def searchAll(word,processors):
+	if processors == 1:
+		results = []
+		os.system("bzgrep '%s' *bz2 > foo" % word)
+		with open('foo','r') as f:
+			for line in f:
+				if len(line) > 0:
+					results.append(json.loads(line.split('bz2:')[1]))
+		return results
+	else:
+		files = [y for x in os.walk('./') for y in glob(os.path.join(x[0], '*.bz2'))]
+		dat = []
+		for file in files:
+			dat.append((word,file))
+
+		p=Pool(processors)
+		results  = p.map(bzgrep,dat)
+		datas = []
+		for result in results:
+			for res in result.split('\n'):
+				try:
+					datas.append(json.loads(res))
+				except:
+					pass
+		return datas
 
 
-results = searchAll('hat')
-#print(results)
+
+print(len(searchAll('hat',1)))
+
+'''
 data = results[0]
 
 cols = []
@@ -74,7 +86,7 @@ for result in results:
 conn.commit()
 c.execute('SELECT body,ups FROM data WHERE ups>100 order by ups desc limit 10')
 print(c.fetchall())
-
+'''
 
 
 
